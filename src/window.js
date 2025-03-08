861cc17689fddec9ce876226d0360030bdbfd9b8
HQH <HQH@NERC-HQH>
2025-03-08 09:16:28 +0800

    删除多余文件

--- src/window.js
+++ src/window.js
@@ -5,6 +5,7 @@ const path = require('path');
 const createSuspensionWindow = (suspensionConfig) => {
   // Create the browser window.
   const win = new BrowserWindow({
+    skipTaskbar: true, // 新增这行关闭任务栏显示
     width: suspensionConfig.width,
     height: suspensionConfig.height,
     type: 'toolbar',
@@ -31,9 +32,39 @@ const createSuspensionWindow = (suspensionConfig) => {
   return win
 };
 
+const createTipWindow = () => {
+  const { left, top } = { left: screen.getPrimaryDisplay().workAreaSize.width - 380, top: screen.getPrimaryDisplay().workAreaSize.height - 820 }
+  //const { left, top } = { left: screen.getPrimaryDisplay().workAreaSize.width - 150, top: screen.getPrimaryDisplay().workAreaSize.height - 100 }
+  const win = new BrowserWindow({
+    skipTaskbar: true, // 新增这行关闭任务栏显示
+    width: 300,
+    minWidth: 300,
+    height: 100,
+    x: left,
+    y: top,
+    frame: false,
+    alwaysOnTop: true,
+    // show: false,
+    // icon: path.join(__dirname, './assets/edit-green.png'),
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+  win.loadFile(path.join(__dirname, 'views/Tip/index.html'));
+  // mainWindow.setBounds({ x: left, y: top, width: suspensionConfig.width, height: suspensionConfig.height })
+  //win.setPosition(left, top)
+  //win.webContents.openDevTools()
+  return win
+}
+
+
 const createEssayWindow = () => {
   const { left, top } = { left: screen.getPrimaryDisplay().workAreaSize.width - 400, top: screen.getPrimaryDisplay().workAreaSize.height - 800 }
   const win = new BrowserWindow({
+    skipTaskbar: true, // 新增这行关闭任务栏显示
     width: 300,
     height: 500,
     minWidth: 300,
@@ -57,6 +88,7 @@ const createEssayWindow = () => {
 const createTodoWindow = () => {
   const { left, top } = { left: screen.getPrimaryDisplay().workAreaSize.width - 380, top: screen.getPrimaryDisplay().workAreaSize.height - 820 }
   const win = new BrowserWindow({
+    skipTaskbar: true, // 新增这行关闭任务栏显示
     width: 300,
     minWidth: 300,
     height: 500,
@@ -77,32 +109,12 @@ const createTodoWindow = () => {
   return win
 }
 
-const createTipWindow = () => {
-  const { left, top } = { left: screen.getPrimaryDisplay().workAreaSize.width - 380, top: screen.getPrimaryDisplay().workAreaSize.height - 820 }
-  const win = new BrowserWindow({
-    width: 300,
-    minWidth: 300,
-    height: 100,
-    x: left,
-    y: top,
-    frame: false,
-    alwaysOnTop: true,
-    // show: false,
-    // icon: path.join(__dirname, './assets/edit-green.png'),
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      enableRemoteModule: true,
-      preload: path.join(__dirname, 'preload.js'),
-    },
-  });
-  win.loadFile(path.join(__dirname, 'views/Tip/index.html'));
-  // win.webContents.openDevTools()
-  return win
-}
+
+
 const createConfigWindow = () => {
   const { left, top } = { left: screen.getPrimaryDisplay().workAreaSize.width - 360, top: screen.getPrimaryDisplay().workAreaSize.height - 840 }
   const win = new BrowserWindow({
+    skipTaskbar: true, // 新增这行关闭任务栏显示
     width: 300,
     minWidth: 300,
     maxWidth: 300,
