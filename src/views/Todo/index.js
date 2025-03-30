2453f4aef134a8b9bc5938092efe7526c68c1d05
eevee <kirby202111@outlook.com>
2025-03-30 23:47:26 +0800

    进度条新增

--- src/views/Todo/index.js
+++ src/views/Todo/index.js
@@ -83,7 +83,19 @@ const app = Vue.createApp({
       dataArray: null,
       canvsanimationFrameId: null,
       isUserStop: false,
-      showProgressInfo: true, 
+      showProgressInfo: true,
+      steps: [
+        {
+          title: '智能会话式编程',
+          status: 'process', // process | error | success
+          message: ''
+        },
+        {
+          title: '智能仿真',
+          status: 'error',
+          message: '环境配置校验失败'
+        }
+      ],
     }
   },
 
